b3d5d470323729ac18e22396f8d9082fada8ca58
Abdul M <120386636+ctrlwinrepair@users.noreply.github.com>
2023-02-05 13:00:12 -0700

    Update VMBackup.sh

--- VMBackup.sh
+++ VMBackup.sh
@@ -2,22 +2,22 @@
 
 echo "VMBackup, back up virtual machines in the most dirty simple way possible"
 
-#An easy script to backup Virtual machines. Replace first and last paths as you see fit
+#An easy script to backup Virtual machines. Replace [ ] (user,Virtual machine image names, first and last paths) as you see fit
 #RUN AS ROOT OBVS
 
-rm /media/luciano/Cryptd/VMBackup/*;
+rm /media/user/[Target device for Backups]/VMBackup/*;
 
 echo "Launching backup now..Please wait..A while. Go make yourself a snack, maybe take nap. Maybe go touch grass..?"
 
-cp -v /var/lib/libvirt/images/debian10-1.qcow2 /media/luciano/Cryptd/VMBackup;
+cp -v /var/lib/libvirt/images/[Virtual Machine Image1].qcow2 /media/[user]/[Target device for Backups]/VMBackup;
 echo "VM 1/5 Complete.."
-cp -v /var/lib/libvirt/images/KaliVM.qcow2 /media/luciano/Cryptd/VMBackup;
+cp -v /var/lib/libvirt/images/[Virtual Machine Image2].qcow2 /media/[user]/[Target device for Backups]/VMBackup;
 echo "VM 2/5 Complete.."
-cp -v /var/lib/libvirt/images/MXLinux.qcow2 /media/luciano/Cryptd/VMBackup;
+cp -v /var/lib/libvirt/images/[Virtual Machine Image3].qcow2 /media/[user]/[Target device for Backups]/VMBackup;
 echo "VM 3/5 Complete.."
-cp -v /var/lib/libvirt/images/ParrotOS.qcow2 /media/luciano/Cryptd/VMBackup;
+cp -v /var/lib/libvirt/images/[Virtual Machine Image4].qcow2 /media/[user]/[Target device for Backups]/VMBackup;
 echo "VM 4/5 Complete.."
-cp -v /var/lib/libvirt/images/Win10Main-clone.qcow2 /media/luciano/Cryptd/VMBackup;
+cp -v /var/lib/libvirt/images/[Virtual Machine Image5].qcow2 /media/[user]/[Target device for Backups]/VMBackup;
 echo "VM 5/5 Complete!"
 echo "Backups done. Thank you for your patience! Have a good day."
 
